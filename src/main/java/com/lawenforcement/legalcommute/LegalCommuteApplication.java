9780416d00e1e33cba4567ce1ca53a7aa96ff6c8
forgotter <melearningcpppart4@gmail.com>
2022-08-14 20:10:15 +0700

    up and running
    ok

---
+++ src/main/java/com/lawenforcement/legalcommute/LegalCommuteApplication.java
@@ -0,0 +1,13 @@
+package com.lawenforcement.legalcommute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalCommuteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegalCommuteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lawenforcement/legalcommute/LegalCommuteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawenforcement.legalcommute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegalCommuteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
