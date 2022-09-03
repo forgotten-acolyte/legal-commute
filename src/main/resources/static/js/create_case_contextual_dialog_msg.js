5c96ad5af4b18e6c804efac417e65810a9d19090
forgotter <melearningcpppart4@gmail.com>
2022-09-03 17:19:08 +0700

    core js to create dynamic HTML elements

--- src/main/resources/static/js/create_case_contextual_dialog_msg.js
+++ src/main/resources/static/js/create_case_contextual_dialog_msg.js
@@ -1,11 +1,29 @@
 $(document).ready(function(){
     let result = $("#postSubmitted").val();
     if(result){
+        // 1. Create the button
+        var button = document.createElement("button");
+        button.setAttribute("data-target", "#myModal");
+        button.setAttribute("data-toggle", "modal");
+        button.setAttribute("id", "dynamicButton");
+
+        // 3. Add event handler
+        // button.addEventListener ("click", function() {
+        //     alert("did something");
+        // });
         let popupName = getSpecificPopupModal(result);
-        $("#myBtn").attr("data-target","#myModal");
-        let stringHTML = '<div th:include= \"'+popupName+'\" class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">\n' + '</div>';
-        $($.parseHTML(stringHTML)).insertAfter($("#insertAfter" ));
-        $("#myBtn").trigger('click');
+
+        // let stringHTML = '<div th:include= \"'+popupName+'\" id="myModal">\n' + '</div>';
+
+        var div = document.createElement("div");
+        div.setAttribute("id", "myModal");
+        div.setAttribute("th:include", popupName);
+
+        var body = document.getElementsByTagName("body")[0];
+        body.appendChild(button);
+        body.appendChild(div);
+
+        $("#dynamicButton").trigger('click');
     }
 });
 
