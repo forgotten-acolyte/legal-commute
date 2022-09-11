e52e56d17058ecc04bb7c19145106b9623f561d9
forgotter <melearningcpppart4@gmail.com>
2022-09-11 15:43:09 +0700

    NEED TO CENTER MODAL POPUP

---
+++ src/main/java/com/lawenforcement/legalcommute/composite_vehicle_offence/offence/model/response/ResultantHtmlContent.java
@@ -0,0 +1,15 @@
+package com.lawenforcement.legalcommute.composite_vehicle_offence.offence.model.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResultantHtmlContent {
+
+    private static final long serialVersionUID = 1L;
+    private String htmlContent;
+}
