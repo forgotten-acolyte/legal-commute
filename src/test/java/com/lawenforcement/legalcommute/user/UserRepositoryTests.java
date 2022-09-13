c1275c17448b7d014b29bcfe7e83e62ccc4f5e70
forgotter <melearningcpppart4@gmail.com>
2022-09-13 20:53:30 +0700

    ok

---
+++ src/test/java/com/lawenforcement/legalcommute/user/UserRepositoryTests.java
@@ -0,0 +1,27 @@
+package com.lawenforcement.legalcommute.user;
+
+import com.lawenforcement.legalcommute.user.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
+public class UserRepositoryTests {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    public void testCreateUser(){
+        testEntityManager.
+
+    }
+}
